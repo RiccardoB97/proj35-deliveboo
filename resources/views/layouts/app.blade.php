86ded4de6787463ffc768bf9108c304040122495
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-09-15 15:51:06 +0200

    Jumbotron fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,11 +39,11 @@
                     {{ config('app.name', 'Laravel') }}
                 </a>
 
-                {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button> --}}
+                </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
